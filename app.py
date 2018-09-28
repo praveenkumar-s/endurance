c977a52cb46cbd11f120397c087142c4b4734fc1
praveenkumar-s <praveenkumar.s@realimage.com>
2018-09-28 13:52:48 +0530

    Adding procfile and reqs

--- app.py
+++ app.py
@@ -1,16 +1,46 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_restplus import Resource, Api
 import firebaseClient as FBC
+from functools import wraps
+import os
+
 
 app = Flask(__name__)
 api = Api(app)
 firebaseInstance=FBC.firebase_client()
-@api.route('/api/<string:data_source>')
+API_KEY=os.environ.get('API_KEY')
+
+def require_appkey(view_function):
+    @wraps(view_function)
+    # the new, post-decoration function. Note *args and **kwargs here.
+    def decorated_function(*args, **kwargs):
+        key=API_KEY
+        #if request.args.get('key') and request.args.get('key') == key:
+        if request.headers.get('x-api-key') and request.headers.get('x-api-key') == key:
+            return view_function(*args, **kwargs)
+        else:
+            abort(401)
+    return decorated_function
+
+
+@api.route('/api/data_store/<string:data_source>')
+
 class readDatafromFB(Resource):
+
+    @require_appkey
+    def post(self,data_source):
+        payload=api.payload
+        try:
+            firebaseInstance.putvalue(child=data_source,data=payload)
+        except:
+            abort(500)
+        return payload,201
+
+
+    @require_appkey
     def get(self,data_source):
-        pass
         return firebaseInstance.getdata(data_source)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
