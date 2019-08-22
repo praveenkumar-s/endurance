20b3248c8485faa643727f15ba11323d76808223
Subramanian <Praveen.Subramanian@kla-tencor.com>
2019-08-22 12:16:50 +0530

    added inclusion

--- SCM.py
+++ SCM.py
@@ -24,4 +24,20 @@ def exclude(data_source,job_name):
     except:
         return "Unable to Complete transaction",500
 
-    
+
+@SCM_blueprint.route("/<data_source>/include/<job_name>", methods =['GET'])
+def include(data_source,job_name):
+    try:
+        token = request.args.get('token')
+        if(not token==os.environ.get('SCM_token')):
+            return "unauthorized =",403
+        
+        existing_data = firebaseInstance.getdata(data_source)
+        if(existing_data is not None)   :
+            existing_data['inclusion']['job_names'].append(job_name)
+            firebaseInstance.putvalue(child=data_source,data=existing_data)
+            return "OK",200
+        else:
+            raise "exce"
+    except:
+        return "Unable to Complete transaction",500    
