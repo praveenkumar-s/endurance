24f3e5057d5a62f2cda948ba148e22bb617c21ff
Subramanian <Praveen.Subramanian@kla-tencor.com>
2019-08-22 12:11:16 +0530

    refactors

--- SCM.py
+++ SCM.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template
 from flask import request
 import os
 import requests
+import firebaseClient as FBC
+firebaseInstance=FBC.firebase_client()
 
 SCM_blueprint = Blueprint('SCM', __name__)
 
@@ -10,13 +12,9 @@ def exclude(data_source,job_name):
     token = request.args.get('token')
     if(not token==os.environ.get('SCM_token')):
         return "unauthorized =",403
-
-    resp = requests.get('http://localhost/api/data_store/{0}'.format(data_source), headers = {'x-api-key': os.environ.get('API_KEY')})
     
-    existing_data = None
-    if(resp.status_code ==200):
-        existing_data = resp.json()
-        existing_data['exclusion']['job_names'].append(job_name)
+    existing_data = firebaseInstance.getdata(data_source)   
+    existing_data['exclusion']['job_names'].append(job_name)
     resp = requests.post("http://ndurance.herokuapp.com/api/data_store/{0}".format(data_source),headers = {'x-api-key': os.environ.get('API_KEY')}, json = existing_data )
     if(resp.status_code==200):
         return "OK",200
